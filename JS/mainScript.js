b9701b8ba60aa0c70617353f39cebedb205c4ea0
Riley Griffis <ragriffis@gmail.com>
2024-12-11 15:37:37 -0500

    Add js for nav

--- JS/mainScript.js
+++ JS/mainScript.js
@@ -1,41 +1,24 @@
+$(window).scroll(function() {
 
-document.addEventListener('DOMContentLoaded', function() {
-        //Adds the nav bar when user starts to scroll
-        if ($(window).width() < 768) {
-            $('#contact-section').addClass('container');
-            $('#contact-section').removeClass('container-fluid');
-        } else {
-            $('#contact-section').addClass('container-fluid');
-            $('#contact-section').removeClass('container');
-        }
-    document.querySelectorAll('.smoothscroll').forEach(anchor => {
-      anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-  
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-  
-        if (targetElement) {
-          targetElement.scrollIntoView({
-            behavior: 'smooth',
-            block: 'center'
-          });
-        }
-      });
-    });
-  });
-  
-/*----------------------------------------------------*/
-/* Quote Loop
------------------------------------------------------- */
+  //Adds the nav bar when user starts to scroll
+  if ($(window).scrollTop() > 50 ) {
+      $('.navbar').addClass('bg-white');
+  } else {
+      $('.navbar').removeClass('bg-white');
+  }
+});
 
-function fade($ele) {
-    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
-        var $next = $(this).next('.quote');
-        fade($next.length > 0 ? $next : $(this).parent().children().first());
-   });
+
+function checkShowClass() {
+  // Get the element with ID 'navbarNav'
+  const navbarNav = document.getElementById('navbarNav');
+
+  // Check if the element has the 'show' class
+  if (navbarNav.classList.contains('show')) {
+  //   if ($(window).scrollTop() < 50 ) {
+  //     $('.navbar').removeClass('bg-white');
+  // } 
+  } else {
+    $('.navbar').addClass('bg-white');
+  }
 }
-fade($('.quoteLoop > .quote').first());
-
-
-
